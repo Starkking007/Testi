119a61fed8abd8e91e7509a2a9e686c2771e89f2
Dropexj <109448302+Dropexj@users.noreply.github.com>
2023-03-11 13:19:06 +0530

    Update play.py

--- AnonX/utils/inline/play.py
+++ AnonX/utils/inline/play.py
@@ -61,7 +61,7 @@ def stream_markup_timer(_, videoid, chat_id, played, dur):
         ],
         [
             InlineKeyboardButton(
-                text="🍷 𝗖𝗵𝗮𝘁 𝗚𝗿𝗼𝘂𝗽🍷", url="https://t.me/WorldChattingFriendsWCF"
+                text="𝘂𝗽𝗱𝗮𝘁𝗲𝘀", url="https://t.me/believerupdate"
             ),
         ],
         [
@@ -123,7 +123,7 @@ def telegram_markup_timer(_, chat_id, played, dur):
         ],
         [
             InlineKeyboardButton(
-                text="🍷 𝗖𝗵𝗮𝘁 𝗚𝗿𝗼𝘂𝗽🍷", url="https://t.me/about_lav"
+                text="𝘂𝗽𝗱𝗮𝘁𝗲𝘀", url="https://t.me/believerupdate"
             ),
         ],
         [
@@ -157,7 +157,7 @@ def stream_markup(_, videoid, chat_id):
         ],
         [
             InlineKeyboardButton(
-                text="🍷 𝗖𝗵𝗮𝘁 𝗚𝗿𝗼𝘂𝗽🍷", url="https://t.me/about_lav"
+                text="𝘂𝗽𝗱𝗮𝘁𝗲𝘀", url="https://t.me/believerupdate"
             ),
         ],
         [
@@ -188,7 +188,7 @@ def telegram_markup(_, chat_id):
         ],
         [
             InlineKeyboardButton(
-                text="🍷 𝗖𝗵𝗮𝘁 𝗚𝗿𝗼𝘂𝗽🍷", url="https://t.me/about_lav"
+                text="𝘂𝗽𝗱𝗮𝘁𝗲𝘀", url="https://t.me/believerupdate"
             ),
         ],
         [
@@ -348,7 +348,7 @@ def queue_markup(_, videoid, chat_id):
         ],
         [
             InlineKeyboardButton(
-                text="🍷 𝗖𝗵𝗮𝘁 𝗚𝗿𝗼𝘂𝗽🍷", url="https://t.me/about_lav"
+                text="𝘂𝗽𝗱𝗮𝘁𝗲𝘀", url="https://t.me/believerupdate"
             ),
         ],
         [
